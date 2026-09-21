d05a51c83f80843117ab6b85eecefbb767c792ea
Ayanraj1234 <2025cs_ayanrajchoudhary_b@nie.ac.in>
2026-09-21 13:19:16 +0530

    Modify the import statement

--- Day/Day 1/P3.py
+++ Day/Day 1/P3.py
@@ -1,2 +1,4 @@
-import math
-print("The square root of 16 is " + str(math.sqrt(16)))
+# import math
+# print("The square root of 16 is " + str(math.sqrt(16)))
+from math import sqrt
+print(sqrt(16))
