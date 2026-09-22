53741d032b37d0b652303786407d198dce0df26e
Ayanraj1234 <2025cs_ayanrajchoudhary_b@nie.ac.in>
2026-09-22 15:19:58 +0530

    Pydantic Model

--- Day/Day 2/FASTAPIPractice/main.py
+++ Day/Day 2/FASTAPIPractice/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -19,3 +20,23 @@ def health():
 @app.post("/create")
 def create_something():
     return {"message":"Created"}
+
+# Path Parameters
+@app.get("/student/{usn}")
+def get_result(usn):
+    return {"Result":"DIstinction","usn":usn}
+
+#  Path Parameters with Type Hints
+@app.get("/candidate/{rollno}")
+def get_candidate(rollno:int):
+    return {"Result":"Distinction","rollno":rollno,"type":str(type(rollno))}
+
+# Pydantic Model
+class Item (BaseModel):
+    name: str
+    price: float
+    is_stock: bool = True
+
+@app.post("/items")
+def create_item(item: Item):
+    return {"received":item, "total_price":item.price*1.18}
